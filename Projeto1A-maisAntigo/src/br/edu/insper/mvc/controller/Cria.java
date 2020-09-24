7f5dd316c3bd6038c7190ee607ddccbdaceee668
Gabriella Cukier <you@example.com>
2020-09-24 09:09:22 -0300

    salvando pastas

---
+++ Projeto1A-maisAntigo/src/br/edu/insper/mvc/controller/Cria.java
@@ -0,0 +1,66 @@
+package br.edu.insper.mvc.controller;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.edu.insper.mvc.model.DAO;
+
+/**
+ * Servlet implementation class Cria
+ */
+@WebServlet("/Cria")
+public class Cria extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Cria() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/view/novoCadastro.jsp");
+		dispatcher.forward(request, response);
+	}
+
+ 	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DAO dao;
+		try {
+			dao = new DAO();
+			Usuario pessoa = new Usuario();
+			 pessoa.setNome(request.getParameter("nome"));
+			 pessoa.setSenha(request.getParameter("senha"));
+			 dao.adiciona(pessoa);
+			 
+			 response.sendRedirect("Login");
+
+			 dao.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		 
+	}
+
+}
